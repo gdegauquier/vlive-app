9b43899b4a71a47196bc3a1914375937526580a6
geoxdd4 <georges-xavier.degauquier@ineat-conseil.fr>
2016-11-28 22:05:15 +0100

    Retrofit

--- V'Live/app/src/main/java/com/vlive/app/ActivityMain.java
+++ V'Live/app/src/main/java/com/vlive/app/ActivityMain.java
@@ -3,17 +3,31 @@ package com.vlive.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivityMain extends AppCompatActivity {
+import com.vlive.pojo.Town;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ActivityMain extends AppCompatActivity implements Callback<ArrayList<Town>> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        //
+    @Override
+    public void onResponse(Call<ArrayList<Town>> call, Response<ArrayList<Town>> response) {
+        //ok
+    }
 
-
-
-
+    @Override
+    public void onFailure(Call<ArrayList<Town>> call, Throwable t) {
+        //ko
     }
 }
+
+// http://www.vogella.com/tutorials/Retrofit/article.html#exercise-using-retrofit-to-query-stackoverflow-in-android
---
+++ V'Live/app/src/main/java/com/vlive/pojo/Town.java
@@ -0,0 +1,18 @@
+package com.vlive.pojo;
+
+/**
+ * Created by Georges on 28/11/2016.
+ */
+
+public class Town {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
